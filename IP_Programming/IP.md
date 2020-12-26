11ec383cf662699ea31d8aa9a66cd9f250d2930f
LFZ <2018110449@mail.scuec.edu.cn>
2020-12-26 11:32:14 +0800

    Initial commit

---
+++ IP_Programming/IP.md
@@ -0,0 +1,28 @@
+# IP
+
+## 一.端口号（重点）port
+
+dest ip(目的ip):
+
+src ip(源ip):
+
+dest port(目标端口)：7788
+
+src port(源端口)：
+
+content： 数据流
+
+
+
+
+
+## 二.socket简介
+
+
+
+
+
+
+
+ 
+
---
+++ IP_Programming/main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# ip地址：192.168.1.1
+# 目的：用来标记网络中的一台电脑，不允许重复
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
